a7cf0396f8376c17162741a84928aac39af4e81d
Яблочкин Андрей Валерьевич <AVYablochkin@sberbank.ru>
2021-11-24 09:25:45 +0300

    -add error logging

--- main_threading.py
+++ main_threading.py
@@ -31,7 +31,7 @@ def do_work(request):
             content = str(response.json()).replace("'", '"')
 
         except Exception as e:
-            print(e)
+            logging.error(f'Error: {e}')
             queue_status = 'ERROR'
             content = "{}"
             resp_sc = '500'
