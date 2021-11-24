55cc6ffa1408acc5664112571e35a34c171a43f8
Яблочкин Андрей Валерьевич <AVYablochkin@sberbank.ru>
2021-11-24 09:16:36 +0300

    -edit url old api

--- connectors/old_api.py
+++ connectors/old_api.py
@@ -11,6 +11,6 @@ req = {'GET': requests.get,
 def old_api_request(endpoint: str, req_type: str, req_body: dict, request_headers: dict) -> requests.Response:
     host = OLD_API_CONFIG['url']
     port = OLD_API_CONFIG['port']
-    url = f'{host}:{port}/{endpoint}'
+    url = f'{host}:{port}{endpoint}'
     response = req[req_type](url=url, data=json.dumps(req_body), headers=request_headers)
     return response
