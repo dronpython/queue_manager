2ae3260d54afcc09e13ea5e1ed40238d8df1cc60
Яблочкин Андрей Валерьевич <AVYablochkin@sberbank.ru>
2022-04-05 16:03:43 +0300

    - format filters null

--- logging_/filters/statusCodeFilter.py
+++ logging_/filters/statusCodeFilter.py
@@ -4,5 +4,5 @@ import logging
 class StatusCodeFilter(logging.Filter):
     """Добавляет источник логов."""
     def filter(self, record):
-        record.status_code = "NULL" if not hasattr(record, "status_code") else record.status_code
+        record.status_code = "null" if not hasattr(record, "status_code") else record.status_code
         return True
--- logging_/filters/systemEndpointFilter.py
+++ logging_/filters/systemEndpointFilter.py
@@ -4,5 +4,5 @@ import logging
 class SystemEndpointFilter(logging.Filter):
     """Добавляет источник логов."""
     def filter(self, record):
-        record.system_ep = "NULL" if not hasattr(record, "system_ep") else record.system_ep
+        record.system_ep = "null" if not hasattr(record, "system_ep") else record.system_ep
         return True
