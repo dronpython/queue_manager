4ed7c0abaacd1aec449ead5deac1dab311075cbf
Яблочкин Андрей Валерьевич <AVYablochkin@sberbank.ru>
2022-09-22 15:38:39 +0300

    add backoff

--- connectors/DBconnector.py
+++ connectors/DBconnector.py
@@ -1,8 +1,54 @@
 import logging
 from psycopg2.extras import NamedTupleCursor
-from psycopg2 import connect, DatabaseError
+from psycopg2 import connect, DatabaseError, OperationalError
 from integration import QUEUE_DB_CONFIG
 from typing import Union
+from functools import wraps
+from time import sleep
+from contextlib import contextmanager
+
+
+def backoff(start_sleep_time=0.1, factor=2, border_sleep_time=10, max_attempt_count=20):
+    """
+    Функция для повторного выполнения функции через некоторое время, если возникла ошибка.
+    Использует наивный экспоненциальный рост времени повтора (factor)
+    до граничного времени ожидания (border_sleep_time)
+
+    Формула:
+        t = start_sleep_time * 2^(n) if t < border_sleep_time
+        t = border_sleep_time if t >= border_sleep_time
+    :param start_sleep_time: начальное время повтора
+    :param factor: во сколько раз нужно увеличить время ожидания
+    :param border_sleep_time: граничное время ожидания
+    :param max_attempt_count: максимальное кол-во попыток
+
+    :return: результат выполнения функции
+    """
+
+    def func_wrapper(func):
+        n = 0
+        t = 0
+
+        @wraps(func)
+        def inner(self, *args, **kwargs):
+            nonlocal n, t
+            a = None
+            while not a:
+                if n == max_attempt_count:
+                    logger.info("Rich max attempt connection count. Close program.")
+                    raise Exception("Connect exception error")
+                try:
+                    logger.info("Trying to connect to db")
+                    a = func(self, **kwargs)
+                    logger.info("DB successfully connected")
+                except OperationalError:
+                    n += 1
+                    t = start_sleep_time * factor ** n if t < border_sleep_time else border_sleep_time
+                    logger.error("Connection error", exc_info=True)
+                    sleep(t)
+            return a
+        return inner
+    return func_wrapper
 
 logger = logging.getLogger(__name__)
 
@@ -30,12 +76,13 @@ class DB:
         self.config = database_config
         self.conn = self._connect()
 
+    @backoff(start_sleep_time=0.2, factor=2, border_sleep_time=10)
+    @contextmanager
     def _connect(self):
-        try:
-            return connect(**self.config)
-        except Exception as error:
-            logger.error(error)
-            return None
+        conn: _connection = connect(**self.config)
+        yield conn
+        logger.info("Closing connection")
+        conn.close()
 
     def select_data(self, table, *args, param_name: Union[str, int] = 1, param_value: Union[str, int] = 1):
         """Выборка записей из базы данных."""
